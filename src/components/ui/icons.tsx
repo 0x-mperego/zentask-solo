c4fe1bd186c8b6ea9945b4ebd382cdafc12a76d9
0x-mperego <dev@mperego.it>
2025-07-01 15:34:44 +0200

    feat(ui): create dedicated icons file and fix command component

---
+++ src/components/ui/icons.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+export function SearchLgIcon(
+  props: React.JSX.IntrinsicAttributes & React.SVGProps<SVGSVGElement>,
+) {
+  return (
+    <svg
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <title>Search Icon</title>
+      <path d="m21 21-6-6m2-5a7 7 0 1 1-14 0 7 7 0 0 1 14 0z" />
+    </svg>
+  );
+} 
